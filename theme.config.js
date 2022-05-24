e56a73ef6c68a20cc4c57d29fb0544ad8d2ad67b
Dan Hu <dan@dansimac.local>
2022-05-24 22:36:45 +0800

    update footer

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,11 @@
 const YEAR = new Date().getFullYear()
 
 export default {
+  github: "https://github.com/dan-hu",
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
       <time>{YEAR}</time> © 只是胡闹.
       <a href="/feed.xml">RSS</a>
-      <a href="https://twitter.com/hu1980">Twitter</a>
-      <a href="https://github.com/dan-hu">Github</a>
       <style jsx>{`
         a {
           float: right;
