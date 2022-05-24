b84cae81bf375575f9cddd41adace07bddc325c2
Dan Hu <dan@dansimac.local>
2022-05-24 21:52:53 +0800

    update footer

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © 只是胡闹.
+      <time>{YEAR}</time> © 只是胡闹 This portfolio is built with Next.js and a library called <a href="https://nextra.vercel.app/" target="_blank">Nextra</a>.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
         a {
