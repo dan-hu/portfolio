37aba044e9bc58e15cde74502d5eb1a7778dc392
Dan Hu <dan@dansimac.local>
2022-05-25 13:32:12 +0800

    update index and page meta

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
-    image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
+    title: '只是胡闹',
+    description: '一个人的胡说八道。',
   }
 
   return (
@@ -12,15 +11,7 @@ export default function Document() {
       <Head>
         <meta name="robots" content="follow, index" />
         <meta name="description" content={meta.description} />
-        <meta property="og:site_name" content={meta.title} />
-        <meta property="og:description" content={meta.description} />
-        <meta property="og:title" content={meta.title} />
-        <meta property="og:image" content={meta.image} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@yourname" />
-        <meta name="twitter:title" content={meta.title} />
-        <meta name="twitter:description" content={meta.description} />
-        <meta name="twitter:image" content={meta.image} />
+        <meta name="twitter:site" content="@hu1980" />
       </Head>
       <body>
         <Main />
